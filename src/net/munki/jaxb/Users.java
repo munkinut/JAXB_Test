d1c415e934f8e69b7d09596d9dcf36a52c73a681
Warren Milburn <warren@munki.net>
2020-05-08 02:27:18 +0100

    Constructed and annotated the Users object.

--- src/net/munki/jaxb/Users.java
+++ src/net/munki/jaxb/Users.java
@@ -1,4 +1,26 @@
 package net.munki.jaxb;
 
+import javax.xml.bind.annotation.XmlElement;
+//import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+//import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement
 public class Users {
+
+    private String user;
+
+    public Users() {
+        user = "";
+    }
+
+    public String getUser() {
+        return this.user;
+    }
+
+    @XmlElement
+    public void setUser(String user) {
+        this.user = user;
+    }
+
 }
